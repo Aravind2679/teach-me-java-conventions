336330f4b838f89cd517a6ee703ddcf9ee741cca
Aravind2679 <aravind102001@gmail.com>
2022-02-10 17:00:11 +0530

    Issue Fixed and Naming conventions changed

---
+++ src/test/java/powerpackage/powerfinderTest.java
@@ -0,0 +1,45 @@
+package powerpackage;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class powerfinderTest {
+
+    @Test
+    @DisplayName("One to the power One")
+    public void oneRaisedToThePowerone()
+    {
+        Assertions.assertEquals(powerfinder.power(1,1),1);
+    }
+
+    @Test
+    @DisplayName("Two to the power One")
+    public void twoRaisedToThePowerone()
+    {
+        Assertions.assertEquals(powerfinder.power(2,1),2);
+    }
+
+    @Test
+    @DisplayName("Two to the power Two")
+    public void twoRaisedToThePowerTwo()
+    {
+        Assertions.assertEquals(powerfinder.power(2,2),4);
+    }
+
+    @Test
+    @DisplayName("Three to the power Two")
+    public void threeRaisedToThePowerTwo()
+    {
+        Assertions.assertEquals(powerfinder.power(3,2),9);
+    }
+
+    @Test
+    public void one_raised_to_one_is_onew() {
+        assert powerfinder.power(1,1) == 1;
+    }
+
+}
